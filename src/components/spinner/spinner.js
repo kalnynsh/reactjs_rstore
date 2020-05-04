4a2bdf1285897354a4a16fb45db8058135e23ed0
kalnynsh <kda869@yandex.ru>
2020-05-04 19:06:58 +0300

    Added simple spinner component.

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+
+    return (
+        <div>
+            ... loading
+        </div>
+    );
+};
+
+export default Spinner;
