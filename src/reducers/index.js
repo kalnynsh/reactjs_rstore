b53ae3def29ea2f7cb7d1b57a8fa5f060102a62a
kalnynsh <kda869@yandex.ru>
2020-05-05 18:14:11 +0300

    Added reducer.

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import {
+    BOOKS_LOADED
+} from '../actions/types';
+
+const initialState = {
+    books: [],
+};
+
+const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case BOOKS_LOADED:
+            return {
+                books: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
