a480ff318dc9ef1cf977e4103b83753d43f75247
kalnynsh <kda869@yandex.ru>
2020-05-05 19:49:06 +0300

    Added elements to BookstoreService.

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -2,6 +2,27 @@
 export default class BookstoreService {
 
     getBooks() {
-        return [];
+        return [
+            {
+                id: 1,
+                title: 'Production-Ready MicroServices',
+                author: 'Susan J. Fowler'
+            },
+            {
+                id: 2,
+                title: 'Learning React',
+                author: 'Kirupa Chinnathathambi'
+            },
+            {
+                id: 3,
+                title: 'React in action',
+                author: 'Mark T. Thomas'
+            },
+            {
+                id: 4,
+                title: 'Release It!',
+                author: 'Michal T. Nygard'
+            },
+        ];
     }
 }
