c2ed2f25d3d143afc12f98654aa6879fef6947df
kalnynsh <kda869@yandex.ru>
2020-05-05 18:14:45 +0300

    Added action booksLoaded.

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import {
+    BOOKS_LOADED
+} from './types';
+
+const booksLoaded = (newBooks) => {
+    return {
+        type: BOOKS_LOADED,
+        payload: newBooks,
+    }
+};
+
+export {
+    booksLoaded
+};
