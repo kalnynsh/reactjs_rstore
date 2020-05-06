2820bc407b1e28ed3e9da034decff5b350a265f1
kalnynsh <kda869@yandex.ru>
2020-05-06 19:40:35 +0300

    Added tasting data to HomePage.

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import BookList from '../book-list';
+import BookstoreService from '../../services/bookstore-service';
 
 const HomePage = () => {
-    return <div>Home Page</div>;
+    const booksService = new BookstoreService();
+    const booksList = booksService.getBooks();
+
+    return (
+        <BookList books={booksList} />
+    );
 };
 
 export default HomePage;
