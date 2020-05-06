f28b2c6ff52faec00fc2bb80d2f6088fc61c830d
kalnynsh <kda869@yandex.ru>
2020-05-06 18:50:51 +0300

    Removed testing withBookstoreService from App.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './app.css';
-import { withBookstoreService } from '../hoc';
 
-const App = ({ bookstoreService }) => {
-
-    console.log(bookstoreService.getBooks());
+const App = () => {
 
     return (
         <div>
@@ -13,4 +10,4 @@ const App = ({ bookstoreService }) => {
     );
 };
 
-export default withBookstoreService()(App);
+export default App;
