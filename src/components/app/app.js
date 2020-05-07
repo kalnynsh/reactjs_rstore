43051d8bfebd785c58073dafa3f423d7509c5655
kalnynsh <kda869@yandex.ru>
2020-05-07 19:58:34 +0300

    Added ShopHeader to App.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { HomePage, CartPage } from '../pages';
+import ShopHeader from '../shop-header';
 import './app.css';
 
 const App = () => {
 
     return (
-        <Switch>
-            <Route
-                path="/"
-                component={HomePage}
-                exact
+        <main role="main" className="container">
+            <ShopHeader numItems={4} total={146} />
+            <Switch>
+                <Route
+                    path="/"
+                    component={HomePage}
+                    exact
+                />
+                <Route
+                    path="/cart"
+                    component={CartPage}
+                />
             />
-            <Route
-                path="/cart"
-                component={CartPage}
-            />
-        />
-        </Switch>
+            </Switch>
+        </main>
     );
 };
 
