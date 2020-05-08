c9a917d78c7e0bd4fb15677f4dd1c40fcdf7d58e
kalnynsh <kda869@yandex.ru>
2020-05-08 19:27:33 +0300

    Added async data loading for BookstoreService.

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,36 +1,42 @@
 
 export default class BookstoreService {
 
+    data = [
+        {
+            id: 1,
+            title: 'Production-Ready MicroServices',
+            author: 'Susan J. Fowler',
+            price: 32,
+            coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg',
+        },
+        {
+            id: 2,
+            title: 'Learning React',
+            author: 'Kirupa Chinnathathambi',
+            price: 41,
+            coverImage: 'https://m.media-amazon.com/images/I/51AFwrzNmdL._AC_UY218_.jpg',
+        },
+        {
+            id: 3,
+            title: 'React in action',
+            author: 'Mark T. Thomas',
+            price: 37,
+            coverImage: 'https://m.media-amazon.com/images/I/71W632wBuWL._AC_UY218_.jpg',
+        },
+        {
+            id: 4,
+            title: 'Release It!',
+            author: 'Michal T. Nygard',
+            price: 36,
+            coverImage: 'https://m.media-amazon.com/images/I/41nMZPJdhsL._AC_UY218_.jpg',
+        },
+    ];
+
     getBooks() {
-        return [
-            {
-                id: 1,
-                title: 'Production-Ready MicroServices',
-                author: 'Susan J. Fowler',
-                price: 32,
-                coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg',
-            },
-            {
-                id: 2,
-                title: 'Learning React',
-                author: 'Kirupa Chinnathathambi',
-                price: 41,
-                coverImage: 'https://m.media-amazon.com/images/I/51AFwrzNmdL._AC_UY218_.jpg',
-            },
-            {
-                id: 3,
-                title: 'React in action',
-                author: 'Mark T. Thomas',
-                price: 37,
-                coverImage: 'https://m.media-amazon.com/images/I/71W632wBuWL._AC_UY218_.jpg',
-            },
-            {
-                id: 4,
-                title: 'Release It!',
-                author: 'Michal T. Nygard',
-                price: 36,
-                coverImage: 'https://m.media-amazon.com/images/I/41nMZPJdhsL._AC_UY218_.jpg',
-            },
-        ];
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(this.data)
+            }, 700);
+        });
     }
 }
