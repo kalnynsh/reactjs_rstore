4c4c769e4517dc424e86308c8101559afb89d484
kalnynsh <kda869@yandex.ru>
2020-05-09 19:32:18 +0300

    Added styles for ErrorIndicator.

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './error-indicator.css';
 
-const ErrorIndicator = () => {
-
+const ErrorIndicator = ({error}) => {
+    console.log(error);
     return (
-        <div>
-            Error!!!
+        <div className="error-message">
+           <span>{error.toString()}</span>
         </div>
     );
 };
