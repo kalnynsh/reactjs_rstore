73f9e9f854568bb35c4365b626c1068a4db78a7b
kalnynsh <kda869@yandex.ru>
2020-05-15 18:05:15 +0300

    Added stringEnhancer for store.

--- src/store.js
+++ src/store.js
@@ -4,6 +4,25 @@ import {
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const stringEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const originalDispatch = store.dispatch;
+
+    store.dispatch = (action) => {
+
+        if (typeof action === 'string') {
+            return originalDispatch({
+                type: action,
+            });
+        }
+
+        return originalDispatch(action);
+    };
+
+    return store;
+};
+
+const store = createStore(reducer, stringEnhancer);
 
 export default store;
